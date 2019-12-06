49f1140c84dc77b9c31230e6f5573955abc8325f
Carla Geglio <carla.geglio@gmail.com>
2019-12-05 23:53:08 -0700

    Add tags property to recipe class

--- src/recipe.js
+++ src/recipe.js
@@ -1,11 +1,9 @@
-const recipeData = require('../data/recipe-data').recipeData;
-const ingredientData = require('../data/ingredient-data');
-
 class Recipe {
   constructor(recipe) {
     this.id = recipe.id;
     this.name = recipe.name;
     this.image = recipe.image;
+    this.tags = recipe.tags;
     this.ingredients = recipe.ingredients;
   }
   calculateIngredientsCost() {
