de10206645dc410175fa0649996ed21417683d28
Carla Geglio <carla.geglio@gmail.com>
2019-12-07 16:38:45 -0700

    Fix linter errors

--- src/scripts.js
+++ src/scripts.js
@@ -3,53 +3,49 @@ let tagList = document.querySelector('.tag-list');
 let recipes = [];
 let filterBtn = document.querySelector(".filter-btn");
 
-
 window.addEventListener("load", createCards);
 window.addEventListener("load", findTags);
 filterBtn.addEventListener("click", findCheckedBoxes);
 
 function createCards() {
-
   recipeData.forEach(recipe => {
     let recipeInfo = new Recipe(recipe);
     recipes.push(recipeInfo);
     if (recipeInfo.name.length > 40) {
       recipeInfo.name = recipeInfo.name.substring(0, 40) + '...';
     }
-  let cardHtml = `
-    <div class="recipe-card" id=${recipeInfo.id}>
-      <h3 maxlength="40">${recipeInfo.name}</h3>
-      <div class="card-photo-preview">
-        <img src=${recipeInfo.image} class="card-photo-preview">
-      </div>
-      <h4>${recipeInfo.tags[0]}</h4>
-      <img src="../images/apple-logo-outline.png" alt="unfilled-apple-icon" class="card-apple-icon">
-    </div>`
+    let cardHtml = `
+      <div class="recipe-card" id=${recipeInfo.id}>
+        <h3 maxlength="40">${recipeInfo.name}</h3>
+        <div class="card-photo-preview">
+          <img src=${recipeInfo.image} class="card-photo-preview">
+        </div>
+        <h4>${recipeInfo.tags[0]}</h4>
+        <img src="../images/apple-logo-outline.png" alt="unfilled-apple-icon" class="card-apple-icon">
+      </div>`
     main.insertAdjacentHTML("beforeend", cardHtml);
   });
-};
-
+}
 
 function findTags() {
   let tags = [];
-  let recipeTags = recipeData.forEach(recipe => {
+  recipeData.forEach(recipe => {
     recipe.tags.forEach(tag => {
       if (!tags.includes(tag)) {
         tags.push(tag);
-      };
+      }
     });
   });
   tags.sort();
   listTags(tags);
-};
+}
 
 function listTags(allTags) {
   allTags.forEach(tag => {
     let tagHtml = `<li><input type="checkbox" class="checked-tag"> ${tag}</li>`;
     tagList.insertAdjacentHTML("beforeend", tagHtml);
   });
-};
-
+}
 
 function findCheckedBoxes() {
   let tagCheckboxes = document.querySelectorAll('.checked-tag');
@@ -58,8 +54,7 @@ function findCheckedBoxes() {
     return box.checked;
   })
   findTaggedRecipes(selectedTags);
-};
-
+}
 
 function findTaggedRecipes(selected) {
   selected.forEach(tag => {
@@ -68,14 +63,14 @@ function findTaggedRecipes(selected) {
     });
     hideUnselectedRecipes(filteredResults);
   });
-};
+}
 
 function hideUnselectedRecipes(filtered) {
-    let foundRecipes = recipes.filter(recipe => {
-      return !filtered.includes(recipe);
-    });
-    foundRecipes.forEach(recipe => {
-      let domRecipe = document.getElementById(`${recipe.id}`);
-      domRecipe.style.display = "none";
-    });
-};
+  let foundRecipes = recipes.filter(recipe => {
+    return !filtered.includes(recipe);
+  });
+  foundRecipes.forEach(recipe => {
+    let domRecipe = document.getElementById(`${recipe.id}`);
+    domRecipe.style.display = "none";
+  });
+}
